ec173b819bdf729626c3ca56335dbaa0d2ffb133
Cameron Bracken <cameron.bracken@gmail.com>
2013-06-11 16:31:07 -0700

    Lots of improvements:     - CSRF tokens     - Flash some useful messages about limiting search results     - Form validation     - Table sorting

--- pitchforksearch/views.py
+++ pitchforksearch/views.py
@@ -12,26 +12,21 @@ import MySQLdb as mdb
 import MySQLdb.cursors as cursors
 
 #TODO 
-# 1. source control
 # 2. sorting 
 # 3. separate artist per table
 # 4. about page
 # 5. message when zero results
-# 6. WTForms
-# 7. sqlalchemy
 
 # mako initilization
 app.template_folder = "templates"
 mako = MakoTemplates(app)
 
+# WTForm class 
 class SearchForm(Form):
     artist = fields.TextField("Artist")
-    score_lower = fields.DecimalField("Score Lower Limit", [validators.NumberRange(min=0.0, max=10.0)], 
-    	places=1, default=0.0)
-    score_upper = fields.DecimalField("Score Upper Limit", [validators.NumberRange(min=0.0, max=10.0)], 
-    	places=1, default=10.0)
-    sort_option = fields.RadioField("Sort by:", choices=[("date_reviewed","Date"),("score","Score")],
-    	default='date_reviewed')
+    score_lower = fields.DecimalField("Score Lower Limit", [validators.NumberRange(min=0.0, max=10.0)], places=1, default=0.0)
+    score_upper = fields.DecimalField("Score Upper Limit", [validators.NumberRange(min=0.0, max=10.0)], places=1, default=10.0)
+    sort_option = fields.RadioField("Sort by:", choices=[("date_reviewed","Date"),("score","Score")], default='date_reviewed')
     table_per_artist = fields.BooleanField("One table per artist")
 
 @app.route('/', methods=("GET", "POST"))
@@ -41,7 +36,7 @@ def index():
 
 	form = SearchForm(csrf_enabled=True)
 	if form.validate_on_submit():
-		flash("Success")
+		#flash("Success")
 
 		columns = ['Score', 'Artist', 'Album', 'BNM', 'Date Reviewed', 'Link']
 	
@@ -53,30 +48,42 @@ def index():
 
 		for artist in artists:
 			q = build_query(artist, form.sort_option.data, form.score_lower.data, form.score_upper.data, limit)
+			q_count = build_query(artist, form.sort_option.data, form.score_lower.data, form.score_upper.data, limit, count_only=True)
+			
 			status,stuff = execute_query(q, cur)
 			#return str(stuff)
 
 			if(status):
 				search_results.append(stuff)
 				titles.append('Results for artist names containing: "%s"' % (artist))
+
+				# never actually return more than limit results, 
+				# but count how many there could have been
+				status,count = execute_query(q_count, cur)
+				if count[0][0] > limit:
+					flash("Only showing %s of %s results, please narrow the search terms." % (limit, count[0][0]))
 			else:
 				return stuff
 
-		return render_template('table.html', result_set = search_results, columns=columns, titles=titles,
-				form=form)
-
+		return render_template('table.html', result_set = search_results, columns=columns, titles=titles, form=form)
 	else:
 		return render_template('layout.html', form=form)
 
-def build_query(artist, sort_option, score_lower, score_upper, limit):
-	q = "select score,artist,album,flag,date_reviewed,url from ratings"
+def build_query(artist, sort_option, score_lower, score_upper, limit, count_only=False):
+	if count_only:
+		q = "select count(*) from ratings"
+	else:
+		q = "select score,artist,album,flag,date_reviewed,url from ratings"
+
 	if(artist == '*'):
 		q += " where"
 	else:
 		q += " where artist like '%%%s%%' and" % (artist)
+
 	q += " score >= %s and score <= %s" % (score_lower, score_upper)
 	q += " order by artist,%s" % (sort_option)
 	q += " limit %d" % limit
+
 	return q
 
 def execute_query(query, cur):
