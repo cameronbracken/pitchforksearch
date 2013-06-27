c901bb9ea5df667a13df8805ee359def8c84a00b
Cameron Bracken <cameron.bracken@gmail.com>
2013-06-27 16:45:31 -0700

    Sorting tips and default sort order, removed sort options from search as that is take care of by tablesorter.

--- pitchforksearch/views.py
+++ pitchforksearch/views.py
@@ -16,7 +16,6 @@ import MySQLdb.cursors as cursors
 # 2. message when zero results
 # 3. search by date
 # 4. no artist defaults to all 
-# 5. Add searching and sorting tips
 # 6. User login for saved searches
 # 7. logging 
 # 8. Spam protection 
@@ -32,7 +31,6 @@ class SearchForm(Form):
     artist = fields.TextField("Artist")
     score_lower = fields.DecimalField("Score Lower Limit", [validators.NumberRange(min=0.0, max=10.0)], places=1, default=0.0)
     score_upper = fields.DecimalField("Score Upper Limit", [validators.NumberRange(min=0.0, max=10.0)], places=1, default=10.0)
-    sort_option = fields.RadioField("Sort by:", choices=[("date_reviewed","Date"),("score","Score")], default='date_reviewed')
 
 @app.route('/', methods=("GET", "POST"))
 def index():
@@ -52,8 +50,8 @@ def index():
 		cur = connect_db()
 
 		for artist in artists:
-			q = build_query(artist, form.sort_option.data, form.score_lower.data, form.score_upper.data, limit)
-			q_count = build_query(artist, form.sort_option.data, form.score_lower.data, form.score_upper.data, limit, count_only=True)
+			q = build_query(artist, form.score_lower.data, form.score_upper.data, limit)
+			q_count = build_query(artist, form.score_lower.data, form.score_upper.data, limit, count_only=True)
 			
 			status,stuff = execute_query(q, cur)
 			#return str(stuff)
@@ -74,7 +72,7 @@ def index():
 	else:
 		return render_template('layout.html', form=form)
 
-def build_query(artist, sort_option, score_lower, score_upper, limit, count_only=False):
+def build_query(artist, score_lower, score_upper, limit, count_only=False):
 	if count_only:
 		q = "select count(*) from ratings"
 	else:
@@ -86,7 +84,7 @@ def build_query(artist, sort_option, score_lower, score_upper, limit, count_only
 		q += " where artist like '%%%s%%' and" % (artist)
 
 	q += " score >= %s and score <= %s" % (score_lower, score_upper)
-	q += " order by artist,%s" % (sort_option)
+	q += " order by artist,score"
 	q += " limit %d" % limit
 
 	return q
